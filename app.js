a680b15a59e4aa65835c3dca5a199d03879deb09
Muhammad Usman <mehr.usman@gmail.com>
2020-06-15 23:23:36 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,608 @@
+// ========= Chapter 1 Question 1
+
+// alert ("Welcome to my Website!");
+
+// ========= Chapter 1 Question 2
+ 
+// alert("Error! Please Enter Valid Password.");  
+
+// ========= Chapter 1 Question 3
+
+// alert('Welcome to JS Land....\nHappy Coding!');
+
+// ========= Chapter 1 Question 4
+
+// alert('Welcome to JS Land....');
+// alert('Happy Coding!');
+
+// ========= Chapter 1 Question 4
+
+//console.log('Hello...I Can run JS through my web browser's console.');
+
+// ========= Chapter 2 Question 1
+
+// var username;
+
+// ========= Chapter 2 Question 2
+
+// var myName = 'Muhammad Usman';
+
+// ========= Chapter 2 Question 3
+
+// var message = 'Hello Wrold';
+// alert(message);
+
+// ========= Chapter 2 Question 4
+
+// var name = 'Jhone Doe';
+// var age = '15 years old';
+// var course = 'Certified Mobile Application Developmet';
+// alert(name);
+// alert(age);
+// alert(course);
+
+// ========= Chapter 2 Question 5
+
+// var display = 'PIZZA\nPIZZ\nPIZ\nPI\nP';
+// alert(display);
+
+// ========= Chapter 2 Question 6
+
+// var email = 'example@example.com';
+// alert('My email address is '+ email);
+
+// ========= Chapter 2 Question 7
+
+// var book = 'A smarter way to learn JavaScript';
+// alert('I am trying to learn from the Book '+ book);
+
+// ========= Chapter 2 Question 8
+
+// document.write('Yah! I can writeHTML content through JavaScript');
+
+// ========= Chapter 2 Question 9
+
+// var display = '▬▬▬▬▬▬▬▬▬ஜ ۩۞۩ ஜ▬▬▬▬▬▬▬▬▬';
+// alert(display);
+// document.write(display);
+
+// ========= Chapter 3 Question 1
+
+// var age = 38;
+// alert('I am  '+ age + ' years old.');
+
+// ========= Chapter 3 Question 2
+
+// var visits = 14;
+// alert('you have visited this wesite  '+ visits + ' times.');
+
+
+// ========= Chapter 3 Question 3
+
+//  var birthYear = 1982;
+//  document.write('My birth year is '+ birthYear + '<br> Datatype of my declared variable is numaric.');
+
+
+// ========= Chapter 3 Question 4
+
+//  var name = 'John Doe';
+//  var product = 'T-shirt';
+//  var quantity = 5;
+//  document.write(name + ' ordered '+ quantity +' ' + product + '(s) on XYZ Clothing store');
+
+// ========= Chapter 4 Question 1
+
+// var x, y, z; 
+
+// ========= Chapter 4 Question 2
+
+// == Legal
+// var man;     
+// var woman3;  
+// var blackDog; 
+// var g3;
+// var sKIES;
+
+// == Illegal
+// var 1due;   
+// var -due;
+// var @due;
+// var Due;
+// var due@1; 
+
+// ========= Chapter 4 Question 3
+
+// document.write('<h1>Display this in your browser</h1><br>');
+// document.write('Variable names can only contain numbers, $ and _ . For example $my_1stVariable<br>');
+// document.write('Variable names are case sensitive<br>');
+// document.write('Variable names should not be JS keywords')
+
+// ========= Chapter 5 Question 1
+
+// var x = 3;
+// var y = 5;
+// document.write('sum of ' + x + ' and ' + y + ' is ' + (x+y) );
+
+
+// ========= Chapter 5 Question 2
+
+// var x = 3;
+// var y = 5;
+// document.write('difference of ' + x + ' and ' + y + ' is ' + (x-y) );
+
+// var x = 3;
+// var y = 5;
+// document.write('multiplication of ' + x + ' and ' + y + ' is ' + (x*y) );
+
+
+// var x = 3;
+// var y = 5;
+// document.write('division of ' + x + ' and ' + y + ' is ' + (x/y) );
+
+
+// var x = 3;
+// var y = 5;
+// document.write('modulus of ' + x + ' and ' + y + ' is ' + (x%y) );
+
+// ========= Chapter 5 Question 3
+
+// var x;
+// document.write('Value after variable declaration is: '+ x + '<br>');
+// x = 5;
+// document.write('Initial value is: '+ x + '<br>');
+// x++;
+// document.write('Value after increment is: '+ x + '<br>');
+// x = x+7;
+// document.write('Value after addition is: '+ x + '<br>');
+// x--;
+// document.write('Value after decrement is: '+ x + '<br>');
+// document.write('The remainder is: '+ x%3 + '<br>');
+
+// ========= Chapter 5 Question 4
+
+// var ticketCost = 600;
+// document.write('Total cost to buy 5 tickets to a movie is ' + (5*ticketCost) + 'PKR');
+
+
+// ========= Chapter 5 Question 5
+
+// var tableNumber = 4;
+
+// document.write('Table of ' + tableNumber + '<br>');
+// document.write('1 X ' + tableNumber + ' = ' + (1*tableNumber) + '<br>');
+// document.write('2 X ' + tableNumber + ' = ' + (2*tableNumber) + '<br>');
+// document.write('3 X ' + tableNumber + ' = ' + (3*tableNumber) + '<br>');
+// document.write('4 X ' + tableNumber + ' = ' + (4*tableNumber) + '<br>');
+// document.write('5 X ' + tableNumber + ' = ' + (5*tableNumber) + '<br>');
+// document.write('6 X ' + tableNumber + ' = ' + (6*tableNumber) + '<br>');
+// document.write('7 X ' + tableNumber + ' = ' + (7*tableNumber) + '<br>');
+// document.write('8 X ' + tableNumber + ' = ' + (8*tableNumber) + '<br>');
+// document.write('9 X ' + tableNumber + ' = ' + (9*tableNumber) + '<br>');
+// document.write('10 X ' + tableNumber + ' = ' + (10*tableNumber) + '<br>');
+
+
+// ========= Chapter 5 Question 6
+
+// var tempC = 25;
+// document.write(tempC + '<sup>o</sup>C is ' +((tempC*9/5)+32)+ '<sup>o</sup>F<br>');
+// var tempF = 70;
+// document.write(tempF + '<sup>o</sup>F is ' +((tempF-32)*5/9)+ '<sup>o</sup>C');
+
+
+// ========= Chapter 5 Question 7
+
+// var price1 = 650;
+// var price2 = 100;
+// var qty1 = 3;
+// var qty2 = 7;
+// var shippingCharges = 100;
+
+// document.write('<h1>Shopping Cart</h1><br>');
+// document.write('Price of item 1 is ' + price1 + '<br>');
+// document.write('Quantity of Item 1 is ' + qty1 + '<br>');
+// document.write('Price of item 2 is ' + price2 + '<br>');
+// document.write('Quantity of Item 2 is ' + qty2 + '<br>');
+// document.write('Shipping Charges ' + shippingCharges + '<br><br>');
+// document.write('Total Cost of your order is ' + ((price1*qty1)+(price2*qty2)+shippingCharges));
+
+// ========= Chapter 5 Question 8
+
+// var totalMarks = 980;
+// var obtainedMarks = 804;
+// document.write('<h1>Marks Sheet</h1><br>');
+// document.write('Total Marks : ' + totalMarks + '<br>');
+// document.write('Marks obtained : ' + obtainedMarks + '<br>');
+// document.write('Percentage : ' + (obtainedMarks/totalMarks)*100 + '%');
+
+// ========= Chapter 5 Question 9
+
+// document.write('<h1>Currencey in PKR</h1><br>Total currencey in PKR is ' + ((10*104.80)+(25*28)));
+
+// ========= Chapter 5 Question 10
+
+// var x = 10;
+
+// document.write(((x+5)*10)/2);
+
+
+// ========= Chapter 5 Question 11
+
+// var cYear = 2016;
+// var bYear = 1992;
+
+// document.write('<h1>Age Calculator</h1><br>Current Year : ' + cYear + '<br>Birth Year : ' + bYear + '<br>Your age is :' + (cYear-bYear));
+
+// ========= Chapter 5 Question 12
+
+// var radius = 20;
+// document.write('<h1>The Geometrizer</h1><br>The circumference is ' + (2*Math.PI*radius) + '<br>The area is ' + (Math.PI*Math.pow(radius, 2)));
+
+
+// ========= Chapter 5 Question 13
+
+// var snak = 'choclate chip';
+// var age = 15;
+// var maxAge = 65;
+// var perDay = 3;
+
+// document.write('<h1>The Lifetime Supply Calculator</h1><br>Favourite Snack : ' + snak + '<br>Current age : ' + age+ '<br>Estimated maximum age : ' + maxAge+ '<br>Amount of snakes per day : ' + perDay+ '<br>You will need  ' + ((maxAge-age)*365*perDay) + ' ' + snak + 'to last you until the ripe old age of ' + maxAge);
+
+
+// ========= Chapter 6-9 Question 1
+
+// var a =  10;
+
+// document.write('Result <br>');
+// document.write('The value of a is : '+ a +' <br>');
+// document.write('================================<br>');
+// document.write('The value of ++a is : '+ (++a) +' <br>');
+// document.write('Now the value of a is : '+ a +' <br><br><br><br>');
+// document.write('The value of a++ is : '+ (a++) +' <br>');
+// document.write('Now the value of a is : '+ a +' <br><br><br><br>');
+// document.write('The value of --a is : '+ (--a) +' <br>');
+// document.write('Now the value of a is : '+ a +' <br><br><br><br>');
+// document.write('The value of a-- is : '+ (a--) +' <br>');
+// document.write('Now the value of a is : '+ a +' <br><br><br><br>');
+
+
+
+// ========= Chapter 6-9 Question 2
+
+// var a = 2, b = 1; 
+// //var result = --a - --b + ++b + b--;
+// var result = --a;
+// document.write('After step --a <br> a :' + a + '<br>b : ' + b + '<br>result :' + result + '<br>');
+// result = result - --b;
+// document.write('After step --a - --b <br>a :' + a + '<br>b : ' + b + '<br>result :' + result + '<br>');
+// result = result + ++b;
+// document.write('After step --a - --b + ++b <br>a :' + a + '<br>b : ' + b + '<br>result :' + result + '<br>');
+// result = result +b--;
+// document.write('After step --a - --b + ++b + b-- <br>a :' + a + '<br>b : ' + b + '<br>result :' + result + '<br>');
+
+
+// ========= Chapter 6-9 Question 3
+
+// var user = prompt("Please enter your name");
+// alert('Welcome ' + user);
+
+// ========= Chapter 6-9 Question 5
+
+// var tableNumber = prompt("Please enter a number",5);
+
+// document.write('Table of ' + tableNumber + '<br>');
+// document.write('1 X ' + tableNumber + ' = ' + (1*tableNumber) + '<br>');
+// document.write('2 X ' + tableNumber + ' = ' + (2*tableNumber) + '<br>');
+// document.write('3 X ' + tableNumber + ' = ' + (3*tableNumber) + '<br>');
+// document.write('4 X ' + tableNumber + ' = ' + (4*tableNumber) + '<br>');
+// document.write('5 X ' + tableNumber + ' = ' + (5*tableNumber) + '<br>');
+// document.write('6 X ' + tableNumber + ' = ' + (6*tableNumber) + '<br>');
+// document.write('7 X ' + tableNumber + ' = ' + (7*tableNumber) + '<br>');
+// document.write('8 X ' + tableNumber + ' = ' + (8*tableNumber) + '<br>');
+// document.write('9 X ' + tableNumber + ' = ' + (9*tableNumber) + '<br>');
+// document.write('10 X ' + tableNumber + ' = ' + (10*tableNumber) + '<br>');
+
+
+// ========= Chapter 6-9 Question 6
+
+// var sub1 = prompt("Please enter subject 1");
+// var sub2 = prompt("Please enter subject 2");
+// var sub3 = prompt("Please enter subject 3");
+
+// var totalMarks = 100;
+
+// var marks1 = prompt("Please enter marks obtained in subject 1");
+// var marks2 = prompt("Please enter marks obtained in subject 2");
+// var marks3 = prompt("Please enter marks obtained in subject 3");
+
+// var totalsum = totalMarks*3;
+// var obtainedsum = number(marks1)+number(marks2)+number(marks3);
+
+// document.write('<table><th><td>Subject</td><td>Total Marks</td><td>Obtained Marks</td><td>Percentage</td></th><tr><td>'+sub1+'</td><td>'+totalMarks+'</td><td>'+ marks1 +'</td><td>'+(marks1/totalMarks)*100+'</td></tr><tr><td>'+sub2+'</td><td>'+totalMarks+'</td><td>'+ marks2 +'</td><td>'+(marks2/totalMarks)*100+'</td></tr><tr><td>'+sub3+'</td><td>'+totalMarks+'</td><td>'+ marks3 +'</td><td>'+(marks3/totalMarks)*100+'</td></tr><tr><td></td><td>'+totalsum+'</td><td>'+ obtainedsum +'</td><td>'+(obtainedsum/totalsum)*100+'</td></tr></table>');
+
+
+// ========= Chapter 9-11 Question 1
+
+//  var city = prompt("Please enter city name :");
+//  if(city=='Karachi'){
+//      alert('Welcome to city of lights.');
+//  }
+
+
+// ========= Chapter 9-11 Question 2
+
+//  var gender = prompt("Please enter your gender (male/female) :");
+//  if(gender=='male'){
+//      alert('Good Morning Sir.');
+//  }else
+    // {
+    //     alert('Good Morning Maam.');
+    // }
+
+// ========= Chapter 9-11 Question 3
+
+//  var color = prompt("Please enter color of Road traffic signal :");
+//  if(color=='Red'){
+//      alert('Must Stop.');
+//  }else if(color=='Yellow')
+//  {
+//     alert('Ready to move.');
+// }else if(color=='Green')
+// {
+//    alert('Move now.');
+// }
+
+// ========= Chapter 9-11 Question 4
+
+//  var fuel = prompt("Please put remaining fuel in car (in litres) :");
+//  if(fuel < 0.25){ 
+//      alert('Please refill the fuel in your car.');
+//  }
+
+// ========= Chapter 9-11 Question 5
+
+// a: value of a is 5 alert displays given condition for variable a is true 
+// var a = 4; 
+// if (++a === 5){ 
+//     alert("given condition for variable a is true"); 
+// }
+
+
+// b: alert does not display value of b is 82
+// var b = 82; 
+// if (b++ === 83){ 
+//     alert("given condition for variable b is true"); 
+// }
+
+// C:   condition 1 if flase because value of c is 12
+//      condition 2 is true and alert dispalys
+//      condition 3 is false because value of c is 14
+//      condition 4 is true because value of c is 14
+// var c = 12; 
+// if (c++ === 13){ 
+//     alert("condition 1 is true"); 
+// } 
+// if (c === 13){ 
+//     alert("condition 2 is true"); 
+// } 
+// if (++c < 14){ 
+//     alert("condition 3 is true"); 
+// } 
+// if(c === 14){ 
+//     alert("condition 4 is true"); 
+// }
+
+// D: alert displays.
+// var materialCost = 20000; 
+// var laborCost = 2000; 
+// var totalCost = materialCost + laborCost; 
+// if (totalCost === laborCost + materialCost){ 
+//     alert("The cost equals"); 
+// }
+
+// e:   first condition is true and alert displays
+//      second condition is alse so alret does not display.
+// if (true){ 
+//     alert("True"); 
+// } 
+// if (false){ 
+//     alert("False"); 
+// }
+
+// f:   condition is true and alert displays
+// if("car" < "cat"){ 
+//     alert("car is smaller than cat"); 
+// }
+
+// ========= Chapter 9-11 Question 6
+
+// var marks = prompt("Please enter marks obtained :");
+// var percentage = (marks/300)*100;
+// if(percentage>=80){
+//     document.write('<h1>Marks Sheet</h1><br>Total Marks : 300<br>Marks Obtained : ' + marks+ '<br>Percentage : ' + percentage+ '%<br>Grade : A-one<br>Remarks : Excelent');
+// }else if(percentage>=70){
+//     document.write('<h1>Marks Sheet</h1><br>Total Marks : 300<br>Marks Obtained : ' + marks+ '<br>Percentage : ' + percentage+ '%<br>Grade : A<br>Remarks : Good');
+// }else if(percentage>=60){
+//     document.write('<h1>Marks Sheet</h1><br>Total Marks : 300<br>Marks Obtained : ' + marks+ '<br>Percentage : ' + percentage+ '%<br>Grade : B<br>Remarks : You need to improve');
+// }else{
+//     document.write('<h1>Marks Sheet</h1><br>Total Marks : 300<br>Marks Obtained : ' + marks+ '<br>Percentage : ' + percentage+ '%<br>Grade : Fail<br>Remarks : Sorry');
+// }
+
+// ========= Chapter 9-11 Question 7
+// var x = 5;
+// var y = prompt("Please enter a number (1-10) :");
+// if(x == y){
+//     alert('Bingo! Correct answer');
+// }else if ((x-1) == y){
+//     alert('Close enough to the correct answer');
+// }
+
+
+// ========= Chapter 9-11 Question 8
+// var x = 6;
+// if(x % 3 == 0){
+//     alert('Disisble by three');
+// }
+
+
+// ========= Chapter 9-11 Question 9
+// var x = prompt("Please enter a number :");
+// if(x % 2 == 0){
+//     alert('Even Number');
+// }else{
+//     alert('Odd Number');
+// }
+
+// ========= Chapter 9-11 Question 10
+
+// var x = prompt("Please enter a temprature :");
+// if(x > 40){
+//     alert('It is too hot outside.');
+// }else if(x > 30){
+//     alert('The Weather today is Normal.');
+// }else if(x > 20){
+//     alert('Today’s Weather is cool.');
+// }else if(x > 10){
+//     alert('OMG! Today’s weather is so Cool.');
+// }
+
+// ========= Chapter 9-11 Question 11
+
+// var num1 = prompt("Please enter first number :");
+// var num2 = prompt("Please enter second number :");
+// var op = prompt("Please enter Operation + - / * :");
+
+// if(op == '+'){
+//     alert('Result : ' + (num1 + num2));
+// }else if(op == '-'){
+//     alert('Result : ' + (num1 - num2));
+// }else if(op == '/'){
+//     alert('Result : ' + (num1 / num2));
+// }else if(op == '*'){
+//     alert('Result : ' + (num1 *num2));
+// }
+
+// ========= Chapter 12-13 Question 1
+// var str = prompt("Please enter a character :");
+// var n = str.charCodeAt(0);
+// if(n>=65 && n<=90){
+//     alert('Upper Case Character');
+// }else if(n>=97 && n<=122){
+//     alert('Lower Case Character');
+// }else if(n>=48 && n<=57){
+//     alert('Numaric Character');
+// }
+
+// ========= Chapter 12-13 Question 2
+// var num1 = prompt("Please enter a number :");
+// var num2 = prompt("Please enter a number :");
+
+// if(num1 == num2){
+//     alert('Numbers are equal');
+// }else if (num1 > num2){
+//     alert(num1);
+// }else{
+//     alert(num2);
+// }
+
+
+// ========= Chapter 12-13 Question 3
+// var num1 = prompt("Please enter a number :");
+
+// if(num1 == 0){
+//     alert('Numbers is sero');
+// }else if (num1 > 0){
+//     alert('number is positive');
+// }else{
+//     alert('Number is negative');
+// }
+
+// ========= Chapter 12-13 Question 4
+// var character = prompt("Please enter a character :");
+
+// if(character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u'){
+//     retuen true;
+// }else
+// {
+//      return false;
+// }
+
+// ========= Chapter 12-13 Question 5
+// var pass1 = 'test';
+// var pass2 = prompt("Please enter password :");
+// if(pass2 == ''){
+//     alert('Please Enter Password');
+// }else if (pass1 == pass2){
+//     alert('Correct! The password you entered matches the original password');
+// }else{
+//     alert('Incorrect password');
+// }
+
+// ========= Chapter 12-13 Question 6
+
+// var greeting; 
+// var hour = 13; 
+// if (hour < 18) {
+//     greeting = "Good day"; 
+// }else {
+//     greeting = "Good evening"; 
+// }
+
+// ========= Chapter 12-13 Question 7
+
+// var time1 = prompt("Please enter time in 24 hour format 1900 :");
+// if(time1 >= '0000' && time1 < '1200'){
+//     alert('Good Morning');
+// }else if(time1 >= '1200' && time1 < '1700'){
+//     alert('Good After Noon');
+// }else if(time1 >= '1700' && time1 < '2100'){
+//     alert('Good Evening');
+// }else if(time1 >= '2100' && time1 < '2359'){
+//     alert('Good Night');
+// }
+
+
+// ========= Chapter 14-16 Question 1
+//var studentName = [];
+
+// ========= Chapter 14-16 Question 2
+//var studentName = new Array();
+
+// ========= Chapter 14-16 Question 3
+//var strArray = ['asdf','ghjk','lopk'];
+
+// ========= Chapter 14-16 Question 4
+//var numArray = [3,4,5,6,7,8,9];
+
+// ========= Chapter 14-16 Question 5
+//var bolArray = [true,false];
+
+// ========= Chapter 14-16 Question 6
+//var mixArray = [1,'asdf',false,8];
+
+// ========= Chapter 14-16 Question 7
+// var qualification = ['SSC', 'HSC', 'BCS', 'BS', 'BCOM', 'MS', 'M. Phil.', 'PhD']
+// document.write('Qualifications : <br>1) ' + qualification[0] + '<br>2)' + qualification[1]+ '<br>3)' + qualification[2]+ '<br>4)' + qualification[3]+ '<br>5)' + qualification[4]+ '<br>6)' + qualification[5]+ '<br>7)' + qualification[6]+ '<br>8)' + qualification[7]);
+
+// ========= Chapter 14-16 Question 8
+// var students = ['Michal','Jhon','Tony'];
+// var marks = [320,230,480];
+
+// document.write('Score of ' + students[0] + ' is ' + marks[0] + '. Percentage :' + marks[0]/500*100 + '%<br>Score of ' + students[1] + ' is ' + marks[1] + '. Percentage :' + marks[1]/500*100 + '%<br>Score of ' + students[2] + ' is ' + marks[2] + '. Percentage :' + marks[2]/500*100 + '%<br>')
+
+
+// ========= Chapter 14-16 Question 10
+// var score = [320,230,480,120];
+// document.write('Scores of students :' + score[0] + ',' +score[1] + ',' + score[2] + ',' + score[3]+'<br>');
+// score.sort();
+// document.write('Scores of students :' + score[0] + ',' +score[1] + ',' + score[2] + ',' + score[3]);
+
+// ========= Chapter 14-16 Question 11
+// var cities = ['Karachi','Lahore','Islamabad','Quetta','Peshawar'];
+// var selectedcities = cities.slice(2,5);
+// document.write('Cities List :<BR>' + cities[0] + ','+ cities[1] + ','+ cities[2] + ','+ cities[3] + ','+ cities[4]);
+// document.write('<br>Selected Cities List :<BR>' + selectedcities[0] + ','+ selectedcities[1] + ','+ selectedcities[2]);
+
+// ========= Chapter 14-16 Question 12
+var strarray = ['This','is','my','cat'];
+document.write('Array : <br>' + strarray.toString());
+document.write('<br>String : <br>' + strarray.join(" "));
